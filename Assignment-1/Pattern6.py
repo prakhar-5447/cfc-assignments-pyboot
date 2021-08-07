3eab5700352d3074b5634f04479474032101a03b
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-07 18:00:45 +0530

    Update Pattern6.py

--- Assignment-1/Pattern6.py
+++ Assignment-1/Pattern6.py
@@ -9,19 +9,18 @@
 #	            *   *	*
 #		            *
 
-row = int(input("Enter number : "))
 i = 1
 k = 1
-while(i<=row):
+while(i<=5):
     sp = 1
-    while(sp<=int(row/2)-k+1):
+    while(sp<=5-k):
         print("\t", end="")
         sp+=1            
     j = 1
     while(j<=2*k-1):
         print("*\t", end="")
         j+=1          
-    if(i<int(row/2)+1):
+    if(i<5):
         k+=1          
     else:
         k-=1         
