921a68711dd2533842b6c56651b4ae41499dd7a1
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-07 15:00:29 +0530

    Delete Assignment-1 directory

--- Assignment-1/ReverseNumber.py
+++
@@ -1,9 +0,0 @@
-     
-Number = int(input("Please Enter any Number: "))    
-Reverse = 0    
-while(Number > 0):    
-    Reminder = Number %10    
-    Reverse = (Reverse *10) + Reminder    
-    Number = Number //10    
-     
-print("Reverse of entered number is = ",Reverse) 
