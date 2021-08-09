0ee8d2e3f2bb6874061d7e9272f3b3fd7cc494ee
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-09 15:11:00 +0530

    Update Conditional.py

--- Assignment-1/Conditional.py
+++ Assignment-1/Conditional.py
@@ -1,3 +1,18 @@
+"""
+Take credits
+If credits is greater than or equal to 7500 then
+Display "Tera Leader"
+Else
+If credits is greater than or equal to 6000 and lesser than 7000 then
+Display "Gega Leader"
+Else
+If credits is greater than or equal to 4500 and lesser than 6000 then
+Display "Mega Leader"
+Else
+otherwise
+Display "Rising Star"
+"""
+
 credits = int(input("ENTER YOUR CREDITS : "))
 if credits>=7500 :
     print("Tera Leader") 
