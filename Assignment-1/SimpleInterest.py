dbb656a12203d23ab8158cdfc6a10d6278e431d1
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-09 15:12:50 +0530

    Update SimpleInterest.py

--- Assignment-1/SimpleInterest.py
+++ Assignment-1/SimpleInterest.py
@@ -1,5 +1,11 @@
-P = int(input("ENTER PRINCIPAL"))
-R = int(input("ENTER RATE"))
-T = int(input("ENTER TIME"))
+"""
+Take Principal, Rate, Time
+Evaluate simple_interest = (P*R*T)/100
+Display simple_interest
+"""
+
+P = int(input("ENTER PRINCIPAL : "))
+R = int(input("ENTER RATE : "))
+T = int(input("ENTER TIME : "))
 Simple_interest = (P * R * T) / 100
-print("The simple interest is: ",Simple_interest)
+print("The simple interest is: "+ Simple_interest)
