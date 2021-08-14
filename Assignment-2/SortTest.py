6df8c00769c1009257c28ef591b56c387f94ce87
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-14 14:04:45 +0530

    Update SortTest.py

--- Assignment-2/SortTest.py
+++ Assignment-2/SortTest.py
@@ -1,10 +1,13 @@
+def SortTest(self):
+    count = 0
+    for i in range (1,len(arr)):
+        if(arr[i - 1] > arr[i]):
+            count += 1
+            break
+    if count==1 :
+        print("NOT SORTED")
+    else :
+        print("SORTED")
+
 arr = [1,2,3,4,5,6,7,1]
-count = 0
-for i in range (1,len(arr)):
-    if(arr[i - 1] > arr[i]):
-        count += 1
-        break
-if count==1 :
-    print("NOT SORTED")
-else :
-    print("SORTED")
+SortTest(arr)
