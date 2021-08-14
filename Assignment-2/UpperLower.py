fed2bbe3a75836a86494c6a2d24e6ba066eefe67
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-08-14 14:05:17 +0530

    Update UpperLower.py

--- Assignment-2/UpperLower.py
+++ Assignment-2/UpperLower.py
@@ -1,7 +1,11 @@
+def uplow(self):
+    if ch > 'A' and ch < 'Z' :
+        print("UPPERCASE")
+    elif ch > 'a' and ch < 'z' :
+        print("LOWERCASE")
+    else :
+        print("WRONG KEY PRESS")
+
+
 ch = input("ENTER ALPHABET : ")
-if ch > 'A' and ch < 'Z' :
-    print("UPPERCASE")
-elif ch > 'a' and ch < 'z' :
-    print("LOWERCASE")
-else :
-    print("WRONG KEY PRESS")
+uplow(ch)
